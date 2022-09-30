36152b7490f2806d7df0a64c23be80b9fb8d4f26
DESKTOP-7CL0DD2\95vin <95vinhndu1415@gmail.com>
2022-09-30 22:32:46 +0700

    push code 30/9/2022

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,33 +1,34 @@
 var showed_box = 0;
 
 nextPartner = () => {
-    showed_box += -20;
+    showed_box += -33.3;
 
-    if (showed_box < -80) showed_box = 0;
+    if (showed_box < -66.6) showed_box = 0;
 
     document.getElementById("sld-partner").style.transform =
         "translateX(" + showed_box + "%)";
 };
 
 previousPartner = () => {
-    showed_box += 20;
+    showed_box += 33.3;
 
-    if (showed_box > 0) showed_box = -80;
+    if (showed_box > 0) showed_box = -66.6;
 
     document.getElementById("sld-partner").style.transform =
         "translateX(" + showed_box + "%)";
 };
+
 nextFeedback = () => {
-    showed_box += -33.333;
-    if (showed_box < -66.666) showed_box = 0;
+    showed_box += -50;
+    if (showed_box < -50) showed_box = 0;
 
     document.getElementById("sld-feedback").style.transform =
         "translateX(" + showed_box + "%)";
 };
 previousFeedBack = () => {
-    showed_box += 33.333;
+    showed_box += 50;
 
-    if (showed_box > 0) showed_box = -66.666;
+    if (showed_box > 0) showed_box = -50;
 
     document.getElementById("sld-feedback").style.transform =
         "translateX(" + showed_box + "%)";
