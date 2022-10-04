f32950cac4365a719338683503144258b9e24c53
cuvii2k <95vinhndu1415@gmail.com>
2022-10-04 14:21:34 +0700

    push done footer

--- JavaScript/dropdown_footer.js
+++ JavaScript/dropdown_footer.js
@@ -1,12 +1,37 @@
 let c = 0;
-function menu(){
+let s = 0;
+function onclickDropdownInfo(){
   if(c % 2 == 0) {
-document.querySelector('.cont_drobpdown_menu').className = "cont_drobpdown_menu active";    
-document.querySelector('.cont_icon_trg').className = "cont_icon_trg active";    
+document.querySelector('.cont_dropdown_info').className = "cont_dropdown_info active-ft";    
+document.querySelector('.cont_icon_info').className = "cont_icon_info active-ft";    
 c++; 
   }else{
-document.querySelector('.cont_drobpdown_menu').className = "cont_drobpdown_menu disable";        
-document.querySelector('.cont_icon_trg').className = "cont_icon_trg disable";        
+document.querySelector('.cont_dropdown_info').className = "cont_dropdown_info disable";        
+document.querySelector('.cont_icon_info').className = "cont_icon_info disable";        
 c++;
+  }
+}
+
+function onclickDropdownService(){
+  if(s % 2 == 0) {
+document.querySelector('.cont_dropdown_service').className = "cont_dropdown_service active-ft";    
+document.querySelector('.cont_icon_service').className = "cont_icon_service active-ft";    
+s++; 
+  }else{
+document.querySelector('.cont_dropdown_service').className = "cont_dropdown_service disable";        
+document.querySelector('.cont_icon_service').className = "cont_icon_service disable";        
+s++;
+  }
+}
+
+function onclickDropdownTrain(){
+  if(s % 2 == 0) {
+document.querySelector('.cont_dropdown_train').className = "cont_dropdown_train active-ft";    
+document.querySelector('.cont_icon_train').className = "cont_icon_train active-ft";    
+s++; 
+  }else{
+document.querySelector('.cont_dropdown_train').className = "cont_dropdown_train disable";        
+document.querySelector('.cont_icon_train').className = "cont_icon_train disable";        
+s++;
   }
 }
