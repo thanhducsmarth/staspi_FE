647354eada9c4579528363a410b740e70f9d4284
cuvii2k <95vinhndu1415@gmail.com>
2022-10-10 08:21:47 +0700

    checkout fist 10/10/2022

--- JavaScript/dropdown_footer.js
+++ JavaScript/dropdown_footer.js
@@ -3,11 +3,11 @@ let s = 0;
 function onclickDropdownInfo(){
   if(c % 2 == 0) {
 document.querySelector('.cont_dropdown_info').className = "cont_dropdown_info active-ft";    
-document.querySelector('.cont_icon_info').className = "cont_icon_info active-ft";    
+document.querySelector('.cont_icon_info').className = "cont_icon_info ti-angle-down";    
 c++; 
   }else{
 document.querySelector('.cont_dropdown_info').className = "cont_dropdown_info disable";        
-document.querySelector('.cont_icon_info').className = "cont_icon_info disable";        
+document.querySelector('.cont_icon_info').className = "cont_icon_info ti-angle-left";        
 c++;
   }
 }
@@ -15,11 +15,11 @@ c++;
 function onclickDropdownService(){
   if(s % 2 == 0) {
 document.querySelector('.cont_dropdown_service').className = "cont_dropdown_service active-ft";    
-document.querySelector('.cont_icon_service').className = "cont_icon_service active-ft";    
+document.querySelector('.cont_icon_service').className = "cont_icon_service ti-angle-down";    
 s++; 
   }else{
 document.querySelector('.cont_dropdown_service').className = "cont_dropdown_service disable";        
-document.querySelector('.cont_icon_service').className = "cont_icon_service disable";        
+document.querySelector('.cont_icon_service').className = "cont_icon_service ti-angle-left";        
 s++;
   }
 }
@@ -27,11 +27,11 @@ s++;
 function onclickDropdownTrain(){
   if(s % 2 == 0) {
 document.querySelector('.cont_dropdown_train').className = "cont_dropdown_train active-ft";    
-document.querySelector('.cont_icon_train').className = "cont_icon_train active-ft";    
+document.querySelector('.cont_icon_train').className = "cont_icon_train ti-angle-down";    
 s++; 
   }else{
 document.querySelector('.cont_dropdown_train').className = "cont_dropdown_train disable";        
-document.querySelector('.cont_icon_train').className = "cont_icon_train disable";        
+document.querySelector('.cont_icon_train').className = "cont_icon_train ti-angle-left";        
 s++;
   }
 }
